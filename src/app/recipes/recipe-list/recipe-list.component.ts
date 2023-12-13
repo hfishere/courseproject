a4c992850e72cb6848194366a8f6f0d5a28eb25d
hfishere <hafidzfairiz@gmail.com>
2023-12-13 18:06:17 +0700

    Initial Commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.less'
+})
+export class RecipeListComponent {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://ladyandtheblog.com/wp-content/uploads/2019/12/Bread8.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://ladyandtheblog.com/wp-content/uploads/2019/12/Bread8.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://ladyandtheblog.com/wp-content/uploads/2019/12/Bread8.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://ladyandtheblog.com/wp-content/uploads/2019/12/Bread8.jpg')
+  ];
+}
