9413949690a277281fe673f3b5fb90818717f06d
hfishere <hafidzfairiz@gmail.com>
2023-12-18 17:10:31 +0700

    Adding Shopping List now works

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -11,4 +11,8 @@ export class ShoppingListComponent {
     new Ingredient('Oranges', 5),
     new Ingredient('Apples', 7)
   ];
+
+  onNewAddedIngredient(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
