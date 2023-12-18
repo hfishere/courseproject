0dab9f8cfcc7e5e78e634e5b2da5d3540c73629c
hfishere <hafidzfairiz@gmail.com>
2023-12-18 16:10:37 +0700

    Make the Recipe Detail works

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,10 +7,15 @@ import { Recipe } from '../recipe.model';
   styleUrl: './recipe-list.component.less'
 })
 export class RecipeListComponent {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   @Input() recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://ladyandtheblog.com/wp-content/uploads/2019/12/Bread8.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://ladyandtheblog.com/wp-content/uploads/2019/12/Bread8.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://ladyandtheblog.com/wp-content/uploads/2019/12/Bread8.jpg'),
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://ladyandtheblog.com/wp-content/uploads/2019/12/Bread8.jpg')
+    new Recipe('A Test Recipe 1', 'This is simply a test 1', 'https://ladyandtheblog.com/wp-content/uploads/2019/12/Bread8.jpg'),
+    new Recipe('A Test Recipe 2', 'This is simply a test 2', 'https://ladyandtheblog.com/wp-content/uploads/2019/12/Bread8.jpg'),
+    new Recipe('A Test Recipe 3', 'This is simply a test 3', 'https://ladyandtheblog.com/wp-content/uploads/2019/12/Bread8.jpg'),
+    new Recipe('A Test Recipe 4', 'This is simply a test 4', 'https://ladyandtheblog.com/wp-content/uploads/2019/12/Bread8.jpg')
   ];
+
+  onSelectedRecipe(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
