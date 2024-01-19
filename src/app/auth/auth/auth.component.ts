b1791bd365a6619aa35137970f1a4289b755066f
Hafidz Fairiz <hafidzfairiz@gmail.com>
2024-01-19 13:59:53 +0000

    Course Checkpoint: Authentication & Route Protection

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -40,8 +40,9 @@ export class AuthComponent {
           console.log(response);
           this.isLoading = false;
         },
-        error => {
-          this.error = 'An error occured!';
+        errorMessage => {
+          console.log(errorMessage)
+          this.error = errorMessage;
           this.isLoading = false;
         }
       );
--- src/app/auth/auth/auth.service.ts
+++ src/app/auth/auth/auth.service.ts
@@ -1,31 +1,49 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable, inject } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { catchError, switchScan, throwError } from 'rxjs';
 
 interface AuthResponseData {
-    kind: string;
-    idToken: string;
-    email: string;
-    refreshToken: string;
-    expiresIn: string;
-    localId: string;
+  kind: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
 }
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
-    private httpClient: HttpClient;
+  private httpClient: HttpClient;
 
-    constructor() {
-        this.httpClient = inject(HttpClient);
-    }
+  constructor() {
+    this.httpClient = inject(HttpClient);
+  }
 
-    signup(email: string, password: string) {
-        return this.httpClient.post<AuthResponseData>(
-            'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAjGenKdEd72j41pD_wXtxzUrobZ-roKqg',
-            {
-                email: email,
-                password: password,
-                returnSecureToken: true
-            }
-        );
-    }
+  signup(email: string, password: string) {
+    return this.httpClient
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAjGenKdEd72j41pD_wXtxzUrobZ-roKqg',
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError((errorResponse) => {
+          let errorMessage = 'An Unknown Error Occurred';
+
+          if (!errorResponse.error || !errorResponse.error.error) {
+            return throwError(errorMessage);
+          }
+
+          switch (errorResponse.error.error.message) {
+            case 'EMAIL_EXISTS':
+              errorMessage = 'Email already exists';
+          }
+
+          return throwError(errorMessage);
+        })
+      );
+  }
 }
